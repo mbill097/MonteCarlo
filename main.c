ab1a9fd710d17691238e1fc0b46a8a159af03e64
mbill <m.billupalli@eagles.oc.edu>
2019-11-20 10:24:00 -0600

    Version 1.3

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <omp.h>
 #include <time.h>
 #include <sys/time.h>
-#define SEED 35791246
+#define SEED 83223123
 
 double Calculate_Pi_Sequential(long int n) {
 
@@ -31,14 +31,14 @@ long double Calculate_Pi_Parallel(long int n) {
     //omp_set_num_threads(4);
 
 #pragma omp for reduction(+:count) private(x,y)
-    for (int i=0; i<n; i++)              //main loop
+    for (int i=0; i<n; i++)              //main for loop
     {
         x = (double)rand()/RAND_MAX;      //gets a random x coordinate
         y = (double)rand()/RAND_MAX;      //gets a random y coordinate
         if (x*x + y*y <= 1.0) count++;
     }
 
-    return (double)count/n*4;
+    return (double)count/n*4; // Return value
 
 }
 
@@ -47,22 +47,20 @@ int main() {
     double sequential_pi, parallel_pi;
     struct timeval start, end;
     long int n;
-    n = 1000000;
+    n = 10000000;
     gettimeofday(&start, NULL);
     printf("Timing sequentiona1...\n");
-    sequential_pi = Calculate_Pi_Sequential(n);
+    sequential_pi = Calculate_Pi_Sequential(n);  // get Sequential value
     gettimeofday(&end, NULL);
     printf("Took %.6f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000L);
     printf("Timing parallel...\n");
     gettimeofday(&start, NULL);
-    parallel_pi = Calculate_Pi_Parallel(n);
+    parallel_pi = Calculate_Pi_Parallel(n); // get Parallel value
     gettimeofday(&end, NULL);
     printf("Took %.6f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000L);
 
-    printf("π = %.10f (sequential)\n", sequential_pi);
-    printf("π = %.10f (parallel)\n", parallel_pi);
+    printf("π = %.10f (sequential)\n", sequential_pi);  // print  sequential value
+    printf("π = %.10f (parallel)\n", parallel_pi); // print  parallel value
 
     return 0;
-    //samples  = 1000000000;
-
 }
