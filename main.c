f6cbba6d65c2f69e14c4975b39f60b4135a6c39f
mbill <m.billupalli@eagles.oc.edu>
2019-11-20 10:19:57 -0600

    Version 1.2

--- main.c
+++ main.c
@@ -3,53 +3,66 @@
 #include <omp.h>
 #include <time.h>
 #include <sys/time.h>
-#include <omp.h>
-
 #define SEED 35791246
-// Returns a random value between -1 and 1
 
+double Calculate_Pi_Sequential(long int n) {
 
-long double Calculate_Pi_Sequential(long long number_of_tosses) {
-    unsigned int seed = (unsigned int) time(NULL);
-    printf("n =" ,number_of_tosses);
-    return 0;
-}
-
-long double Calculate_Pi_Parallel(long long number_of_tosses) {
-#pragma omp1 parallel num_threads(omp_get_max_threads())
-    {
-        unsigned int seed = (unsigned int) time(NULL) + (unsigned int) omp_get_thread_num();
-        printf("n1 =" ,number_of_tosses);
+    srand(SEED);
+    long count;
+    double x, y;
+    count = 0;
+    for(int i=0;i<n;i++){
+        x=(double)rand()/RAND_MAX;
+        y=(double)rand()/RAND_MAX;
+        if (x*x + y*y <= 1.0)
+        {
+            count++;
+        }
     }
-    return 0;
+    return (double)count/n*4;
 }
 
-int main(int argc, char *argv[])
-{
-   int niter=3;
-   double xCoordinate,yCoordinate;
-   int i,count=0; /* # of points in the 1st quadrant of unit circle */
-   double z;
-   double pi;
-  /* printf("This program to calculate the value of the mathematical constant PI using the Monte Carlo method.");
-   printf("Enter the number of iterations used to estimate pi: ");
-   scanf("%d",&niter);*/
-   /* initialize random numbers */
-   /*get rand values */
-   rand();
-   count=0;
-   for (i=0; i<niter; i++) {
-    xCoordinate = (double)rand()/RAND_MAX;
-    yCoordinate = (double)rand()/RAND_MAX;
-    z = xCoordinate*xCoordinate+yCoordinate*yCoordinate;
-    if (z<=1) count++;
-   }
+long count;
 
-long double sequential_pi = Calculate_Pi_Sequential(niter);
-long double parallel_pi = Calculate_Pi_Parallel(niter);
-pi=(double)count/niter*4;
-printf("# of trials= %d , estimate of pi is %g \n",niter,pi);
-    printf("π = %.10Lf (sequential)\n", sequential_pi);
-    printf("π = %.10Lf (parallel)", parallel_pi);
+long double Calculate_Pi_Parallel(long int n) {
+
+    double x, y;
+    srand(SEED);
+    //omp_set_num_threads(4);
+
+#pragma omp for reduction(+:count) private(x,y)
+    for (int i=0; i<n; i++)              //main loop
+    {
+        x = (double)rand()/RAND_MAX;      //gets a random x coordinate
+        y = (double)rand()/RAND_MAX;      //gets a random y coordinate
+        if (x*x + y*y <= 1.0) count++;
+    }
+
+    return (double)count/n*4;
+
+}
+
+int main() {
+
+    double sequential_pi, parallel_pi;
+    struct timeval start, end;
+    long int n;
+    n = 1000000;
+    gettimeofday(&start, NULL);
+    printf("Timing sequentiona1...\n");
+    sequential_pi = Calculate_Pi_Sequential(n);
+    gettimeofday(&end, NULL);
+    printf("Took %.6f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000L);
+    printf("Timing parallel...\n");
+    gettimeofday(&start, NULL);
+    parallel_pi = Calculate_Pi_Parallel(n);
+    gettimeofday(&end, NULL);
+    printf("Took %.6f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000L);
+
+    printf("π = %.10f (sequential)\n", sequential_pi);
+    printf("π = %.10f (parallel)\n", parallel_pi);
+
     return 0;
+    //samples  = 1000000000;
+
 }
